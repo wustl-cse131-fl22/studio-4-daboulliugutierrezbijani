d2b7d3c6507678409cfd929a97ab01986eee1e4d
zeinadaboul <zeinadaboul@localhost>
2022-10-12 15:48:46 -0500

    Japan Zindabad

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -1,11 +1,30 @@
 package studio4;
 
 import java.awt.Color;
+import java.io.File;
+import java.util.Scanner;
+
+import javax.swing.JFileChooser;
 
 import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
 		
+		
+//		String y3 = in.next();
+		//String halfWidth = in.next();
+		//String halfHeight = in.next();
+	
+		
+		
+	
+		//StdDraw.rectangle(Double.valueOf(x),Double.valueOf(y), Double.valueOf(halfWidth), Double.valueOf(halfHeight));
+		
+//		StdDraw.setPenColor(Integer.valueOf(redComponent), Integer.valueOf(greenComponent), Integer.valueOf(blueComponent));
+//		StdDraw.filledRectangle(Double.valueOf(x),Double.valueOf(y), Double.valueOf(halfWidth), Double.valueOf(halfHeight));
+		
+		StdDraw.setPenColor(Integer.valueOf(redComponent), Integer.valueOf(greenComponent), Integer.valueOf(blueComponent));
+		StdDraw.filledEllipse(Double.valueOf(x),Double.valueOf(y), Double.valueOf(semiMajorAxis), Double.valueOf(semiMinorAxis));
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -20,5 +20,35 @@ public class InterpretDrawingFile {
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
 		
+		String shapeType = in.next();
+		String redComponent = in.next();
+		String greenComponent = in.next();
+		String blueComponent = in.next();
+		String isFilled = in.next();
+		String x = in.next();
+		String y = in.next();
+		String semiMajorAxis = in.next();
+		String semiMinorAxis = in.next();
+//		String x3 = in.next();
+//		String y3 = in.next();
+		//String halfWidth = in.next();
+		//String halfHeight = in.next();
+	
+		
+		
+	
+		//StdDraw.rectangle(Double.valueOf(x),Double.valueOf(y), Double.valueOf(halfWidth), Double.valueOf(halfHeight));
+		
+//		StdDraw.setPenColor(Integer.valueOf(redComponent), Integer.valueOf(greenComponent), Integer.valueOf(blueComponent));
+//		StdDraw.filledRectangle(Double.valueOf(x),Double.valueOf(y), Double.valueOf(halfWidth), Double.valueOf(halfHeight));
+		
+		StdDraw.setPenColor(Integer.valueOf(redComponent), Integer.valueOf(greenComponent), Integer.valueOf(blueComponent));
+		StdDraw.filledEllipse(Double.valueOf(x),Double.valueOf(y), Double.valueOf(semiMajorAxis), Double.valueOf(semiMinorAxis));
+
+		
+		
+		
+		
+		
 	}
 }
